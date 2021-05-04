17d1a5fa7ee84f42169a73037f2c80a4604b2e0c
zwergius <webmaster@zwergius.com>
2021-05-04 08:36:52 +0200

    Initial commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function load() {
+		return { props: { html: '<p>hello weird unrecoverable error in Root</p>' } };
+	}
+</script>
+
+<script>
+	export let html;
+</script>
+
+<slot />
+<footer>{@html html}</footer>
